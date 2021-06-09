503186ee64ed9666e43695f1c26fb4e3ea52233f
nezarra <neselvia@gmail.com>
2021-06-09 18:59:31 +0700

    E31192159

---
+++ APIvolley/app/src/main/java/com/example/apivolley/Util/ServerAPI.java
@@ -0,0 +1,11 @@
+package com.example.apivolley.Util;
+
+/**
+ * Setting alamat API CRUD yang telah dibuat pada CodeIgniter
+ */
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.1.8/ci/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.1.8/ci/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.1.8/ci/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.1.8/ci/Mahasiswa/ApiUpdate";
+}
---
+++ APIvolley/app/src/main/java/com/example/apivolley/layout_row.java
@@ -0,0 +1,14 @@
+package com.example.apivolley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layout_row extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_row);
+    }
+}
